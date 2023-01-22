8e86d1175d4fc5236f4ab3e35f824b03600a7c90
Imtiaz Shawn <Imtiazshawn01@gmail.com>
2023-01-22 20:15:54 +0600

    update url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // Fetch data from server
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://dott-bot.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
